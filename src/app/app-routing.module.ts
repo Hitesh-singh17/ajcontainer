b388afe0840c27c92047457e7772cee7a9033c79
Hitesh Singh <70806394+Hitesh-singh17@users.noreply.github.com>
2024-03-25 15:39:48 +0530

    added new component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloworldComponent } from './helloworld/helloworld.component';
 
 const routes: Routes = [
   {
     path: 'home', loadChildren: async () => (await import('./librarayname1/libraray1/libraray1.module')).Libraray1ListModule
     // path: 'accessservices', loadChildren: async () => (await import('./accessservices/accessservices.module')).Libraray1Module
   },
+  {
+    path:'',component: HelloworldComponent
+  }
 ];
 
 @NgModule({
