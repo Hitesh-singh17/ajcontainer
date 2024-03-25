08315296316735facc281045cfb262b6e51ba837
Hitesh Singh <70806394+Hitesh-singh17@users.noreply.github.com>
2024-03-25 19:49:39 +0530

    changed url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,13 @@ const routes: Routes = [
     // path: 'accessservices', loadChildren: async () => (await import('./accessservices/accessservices.module')).Libraray1Module
   },
   {
-    path:'',component: HelloworldComponent
+    path:'hello',component: HelloworldComponent,
+    
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
